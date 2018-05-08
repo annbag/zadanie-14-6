41effde0c9b95b60e031391d6d667adb2ff73c8a
Anna Bagrowska <anna_bagrowska@wp.pl>
2018-05-08 22:08:49 +0200

    add component lifecycle

--- script.js
+++ script.js
@@ -1,9 +1,18 @@
 var Counter = React.createClass({
+    getDefaultProps: function () {
+        console.log ('Faza inicjalizacji')
+    },
+
     getInitialState: function() {
+        console.log('Faza inicjalizacji, początkowy stan naszego komponentu')
         return {
             counter: 0
         };
     },
+
+    componentWillMount: function () {
+        console.log('Metoda wywoływana tylko w momencie inicjalizacji komoponentu')
+    },    
 
     decrement : function() {
         this.setState({
@@ -16,15 +25,41 @@ var Counter = React.createClass({
             counter: this.state.counter + 1
         });
     },
-
-
     render: function() {
+        console.log()
         return React.createElement('div', {},
             React.createElement('span', {}, 'Licznik ' + this.state.counter),
             React.createElement('button', {onClick: this.decrement}, 'Decrement'),
             React.createElement('button', {onClick: this.increment}, 'Increment'),
         );
-    }
+    },
+
+    componentDidMount: function () {
+        console.log('W tym miejscu możemy wykonać rózne operacje monipulacji, użyć jQuery lub pobrać dane')
+    },
+
+    componentWillReceiveProps: function () {
+        console.log('Faza aktualizacji, zosatje wywołana, gdy komponent otrzyma nowe właściwości, aktualizowanie stanu na podsitwie nadchodzących właściwości')
+    },
+
+    shouldComponentUpdate: function () {
+        console.log('Faza aktualizacji, pozwala sprawdzić czy faktycznie trzeba jeszcze raz przeryswoać komponent, zwraca wartośc true lub false')
+        return true;
+    },
+
+    componentWillUpdate: function () {
+        console.log('Faza aktualizacji, zosatnie wywyołana jeśli wcześniejsza metoda zwraca true')
+    },
+
+    componentDidUpdate: function () {
+        console.log('Faza aktualizacji, można wykonać np jakieś manipulacje DOM')
+    },
+
+    componentWillUnmount: function () {
+        console.log('faza usuwania')
+    },
+
+
 });
 
 
